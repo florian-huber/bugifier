f2343bf50df2175b5748cccacf293e8662112f6a
florian-huber <florian.huber@hs-duesseldorf.de>
2022-09-11 21:01:49 +0200

    some first dummy code

--- bugifier/script_runner.py
+++ bugifier/script_runner.py
@@ -1,2 +1,9 @@
 def python_script_runner(filename):
     exec(open(filename).read())
+
+    all_variables = dir()
+    variable_names = []
+    for name in all_variables:
+        if not name.startswith('__'):
+            variable_names.append(name)
+    return variable_names
--- tests/test_script_runner.py
+++ tests/test_script_runner.py
@@ -1,9 +1,10 @@
 import os
-from bugifier import python_script_runner
+from ..bugifier import python_script_runner
 
 
 filename = os.path.join(os.getcwd(), "tests", "example_script.py")
 
 
 def test_script_runner():
-    python_script_runner(filename)
+    variable_names = python_script_runner(filename)
+    assert variable_names == ["filename", "variable1", "variable2"]
